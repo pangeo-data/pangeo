534922b641f8e826061065ba7fd83b64493bece9
Joseph Hamman <jhamman@ucar.edu>
2017-11-06 14:37:27 -0700

    use nanny feature and lower memory limit

--- utilities/cheyenne/launch-dask-scheduler.sh
+++ utilities/cheyenne/launch-dask-scheduler.sh
@@ -2,7 +2,7 @@
 #PBS -N dask-scheduler
 #PBS -q economy
 #PBS -A UCLB0022
-#PBS -l select=1:ncpus=36:mpiprocs=6:ompthreads=6
+#PBS -l select=1:ncpus=36:mpiprocs=9:ompthreads=4:mem=109GB
 #PBS -l walltime=11:59:00
 #PBS -j oe
 #PBS -m abe
@@ -18,5 +18,8 @@ source activate pangeo
 
 SCHEDULER=/glade/scratch/$USER/scheduler.json
 rm -f $SCHEDULER
-mpirun --np 6 dask-mpi --nthreads 6 --memory-limit 22e9 --interface ib0 \
-    --local-directory /glade/scratch/$USER --scheduler-file=$SCHEDULER
+mpirun --np 9 dask-mpi --nthreads 4 \
+    --memory-limit 12e9 --nanny \
+    --interface ib0 \
+    --local-directory /glade/scratch/$USER \
+    --scheduler-file=$SCHEDULER
