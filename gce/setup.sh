7171fd3d1ebed67f34bd75fc7221c3e37e77ee60
Matthew Rocklin <mrocklin@gmail.com>
2018-01-19 09:05:32 -0500

    Add basic JupyterHub + Dask deployment (#57)
    * Add basic JupyterHub + Dask deployment
    
    * add pip/conda packages
    
    * Add xarray, netCDF, scipy to docker images
    
    * add admin users
    
    * Add fuse to notebook image
    
    * update config files
    
    * add sudo to pangeo-notebook
    
    * update config
    
    * add jedi to notebook
    
    * Redesign docker files
    
    This no longer relies on the dask-foo images.
    Fuse now works on the notebook and worker image
    Adds .daskernetes.yaml file to define workers
    
    * add a few missing libraries found when using Zarr
    
    Also add a --death-timeout for the workers to clean them up
    
    * clean up notebook docker image
    
    * add matplotlib
    
    * add notebook prepare script and notebook
    
    * add --sys-prefix to nbsererproxy install
    
    * add notebooks
    
    * cull users after 10m
    
    * remove dask helm values.yaml
    
    * update pangeo gce instructions

---
+++ gce/setup.sh
@@ -0,0 +1,29 @@
+# Start cluster on Google cloud
+gcloud container clusters create pangeo --num-nodes=10 --machine-type=n1-standard-2 --zone=us-central1-b  --cluster-version=1.8.4-gke.1
+gcloud container clusters get-credentials pangeo --zone us-central1-b --project pangeo-181919
+
+# Set up Kubernetes
+kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=mrocklin@gmail.com
+kubectl --namespace kube-system create sa tiller
+kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+helm init --service-account tiller
+kubectl --namespace=kube-system patch deployment tiller-deploy --type=json --patch='[{"op": "add", "path": "/spec/template/spec/containers/0/command", "value": ["/tiller", "--listen=localhost:44134"]}]'
+
+# Get Helm repositories
+helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
+helm repo update
+
+# Install JupyterHub and Dask on the cluster
+helm install jupyterhub/jupyterhub --version=v0.6.0-9701a90 --name=jupyter --namespace=pangeo -f jupyter-config.yaml
+helm install dask/dask --name=dask --namespace=pangeo -f dask-config.yaml
+
+# Look for publised services.  Route domain name A records to these IPs.
+kubectl get services --namespace pangeo
+# Look for dask-scheduler
+#      and proxy-...
+
+
+# In notebooks, connect to the Dask cluster like so:
+# from dask.distributed import Client
+# client = Client()
+# client
