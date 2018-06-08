bc3dc137cb4fb0e3809a580f7dba39a79f67819d
Joe Hamman <jhamman@ucar.edu>
2018-06-08 09:54:03 -0400

    Little fixes (#300)
    * copyright 2018
    
    * fix a typo

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Pangeo'
-copyright = '2017, Pangeo Team'
+copyright = '2018, Pangeo Team'
 author = 'Pangeo Team'
 
 # The version info for the project you're documenting, acts as replacement for
