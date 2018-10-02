109ce53d539a854f13447d5f8d34b663b9673142
Ariel Rokem <arokem@gmail.com>
2018-10-02 08:51:05 -0700

    Fix gard-coded absolute path => relative path.

--- gce/setup-guide/4_upgrade_helm.sh
+++ gce/setup-guide/4_upgrade_helm.sh
@@ -4,5 +4,5 @@ set -e
 
 helm upgrade --force --recreate-pods --devel pangeohub pangeo/pangeo \
 #   --version=$VERSION \
-   -f /opt/pangeo/secret_config.yaml \
-   -f /opt/pangeo/jupyter_config.yaml
+   -f secret_config.yaml \
+   -f jupyter_config.yaml
