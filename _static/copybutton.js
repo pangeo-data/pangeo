543e39a706243a49f108c7fca96a6a0b8734b51f
GitHub Action <action@github.com>
2020-12-08 18:51:03 +0000

    Update documentation

--- _static/copybutton.js
+++ _static/copybutton.js
@@ -68,6 +68,7 @@ const addCopyButtonToCodeCells = () => {
     return
   }
 
+  // Add copybuttons to all of our code cells
   const codeCells = document.querySelectorAll('div.highlight pre')
   codeCells.forEach((codeCell, index) => {
     const id = codeCellId(index)
@@ -76,12 +77,69 @@ const addCopyButtonToCodeCells = () => {
 
     const clipboardButton = id =>
     `<a class="copybtn o-tooltip--left" style="background-color: ${pre_bg}" data-tooltip="${messages[locale]['copy']}" data-clipboard-target="#${id}">
-      <img src="https://gitcdn.xyz/repo/choldgraf/sphinx-copybutton/master/sphinx_copybutton/_static/copy-button.svg" alt="${messages[locale]['copy_to_clipboard']}">
+      <img src="${DOCUMENTATION_OPTIONS.URL_ROOT}_static/copy-button.svg" alt="${messages[locale]['copy_to_clipboard']}">
     </a>`
     codeCell.insertAdjacentHTML('afterend', clipboardButton(id))
   })
 
-  const clipboard = new ClipboardJS('.copybtn')
+function escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+
+// Callback when a copy button is clicked. Will be passed the node that was clicked
+// should then grab the text and replace pieces of text that shouldn't be used in output
+function formatCopyText(textContent, copybuttonPromptText, isRegexp = false, onlyCopyPromptLines = true, removePrompts = true) {
+
+    var regexp;
+    var match;
+
+    // create regexp to capture prompt and remaining line
+    if (isRegexp) {
+        regexp = new RegExp('^(' + copybuttonPromptText + ')(.*)')
+    } else {
+        regexp = new RegExp('^(' + escapeRegExp(copybuttonPromptText) + ')(.*)')
+    }
+
+    const outputLines = [];
+    var promptFound = false;
+    for (const line of textContent.split('\n')) {
+        match = line.match(regexp)
+        if (match) {
+            promptFound = true
+            if (removePrompts) {
+                outputLines.push(match[2])
+            } else {
+                outputLines.push(line)
+            }
+        } else {
+            if (!onlyCopyPromptLines) {
+                outputLines.push(line)
+            }
+        }
+    }
+
+    // If no lines with the prompt were found then just use original lines
+    if (promptFound) {
+        textContent = outputLines.join('\n');
+    }
+
+    // Remove a trailing newline to avoid auto-running when pasting
+    if (textContent.endsWith("\n")) {
+        textContent = textContent.slice(0, -1)
+    }
+    return textContent
+}
+
+
+var copyTargetText = (trigger) => {
+  var target = document.querySelector(trigger.attributes['data-clipboard-target'].value);
+  return formatCopyText(target.innerText, '', false, true,  true)
+}
+
+  // Initialize with a callback so we can modify the text before copy
+  const clipboard = new ClipboardJS('.copybtn', {text: copyTargetText})
+
+  // Update UI with error/success messages
   clipboard.on('success', event => {
     clearSelection()
     temporarilyChangeTooltip(event.trigger, messages[locale]['copy_success'])
